0cc1f2493bcf30229aa28f065c1a8c617abcb1cd
Enes KOC <eneskoc.0063@gmail.com>
2025-01-10 13:02:26 +0300

    Second Commit

---
+++ Job.py
@@ -0,0 +1,3 @@
+class Job:
+    def __int__(self, job_id):
+        self.__job_id = job_id
---
+++ Procs.py
@@ -0,0 +1,5 @@
+class Procs:
+    def __int__(self, operation_id, machine_id, duration):
+        self.__operation_id = operation_id
+        self.__machine_id = machine_id
+        self.__duration = duration
